32dd4c27effd66c4473b9267cb77cebd501fd5cb
Tareq Alayan <talayan@talayan-laptop2.tlv.redhat.com>
2015-11-02 16:12:19 +0200

    split_reviews: stupid fix

--- split-reviews/split_reviews.py
+++ split-reviews/split_reviews.py
@@ -249,7 +249,7 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 1:
         main(sys.argv[1:])
     else:
         print 'Try %s -h' % sys.argv[0]
