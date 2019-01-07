13591ed8661330cd0b34f46bb89c5552c3293172
Tareq Alayan <talayan@redhat.com>
2019-01-07 14:21:46 +0200

    token_tokem: don't encode pin to simplify usage

--- otp_generator/token_token.py
+++ otp_generator/token_token.py
@@ -21,7 +21,7 @@ from subprocess import Popen, PIPE
 #
 #secret = PCTRRUH75NL5QQUUE2EDQALHNM7KQALL
 ##############################################################################
-DEFAULT_CONF_FILE = 'token.conf'
+DEFAULT_CONF_FILE = '.token.conf'
 
 class Token(object):
     def __init__(self, conf=DEFAULT_CONF_FILE):
@@ -51,17 +51,17 @@ class Token(object):
     def copy_to_clipboard(self, with_pin=False):
         p = Popen(["xsel","-bi"], stdin=PIPE)
         if with_pin:
-            p.communicate(input='%s%s' % (self.get_decoded_pin(), self.token))
+            p.communicate(input='%s%s' % (self.pin, self.token))
         else:
             p.communicate(input=str(self.token))
 
-    def set_pin_encoded(self, pin):
-        self.pin = pin.encode('base64', 'strict')
-        self.config['pin'] = self.pin
-        self.config.write()
+    # def set_pin_encoded(self, pin):
+    #     self.pin = pin.encode('base64', 'strict')
+    #     self.config['pin'] = self.pin
+    #     self.config.write()
 
-    def get_decoded_pin(self):
-        return self.pin.decode('base64', 'strict')
+    # def get_decoded_pin(self):
+    #     return self.pin.decode('base64', 'strict')
 
     def print_token(self, with_pin=False):
         print self.token
@@ -72,7 +72,7 @@ def print_usage_and_die(exit_code=0):
     print 'token.py --xsel-with-pin:  will copy $PIN$OTP to clip board'
     print 'token.py: print OTP to stdout'
     print 'token.py --print-with-pin: will print $PIN$OTP to stdout'
-    print 'token.py -s <pin>: will set your pin encoded into ~/token.conf'
+    # print 'token.py -s <pin>: will set your pin encoded into ~/token.conf'
     print 'token.py -h: will print the message'
     sys.exit(exit_code)
 
@@ -104,8 +104,8 @@ if __name__ == "__main__":
             t.copy_to_clipboard(with_pin=True)
         elif opt in ('--print-with-pin'):
             t.print_token()
-        elif opt in ('--setpin', '-s'):
-            print('You have your PIN encoded in ~/.token.conf')
-            t.set_pin_encoded(arg)
+        # elif opt in ('--setpin', '-s'):
+        #     print('You have your PIN encoded in ~/.token.conf')
+        #     t.set_pin_encoded(arg)
         else:
             print_usage_and_die()
